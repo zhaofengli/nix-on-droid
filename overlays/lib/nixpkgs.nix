118df634564f1e79d8d899ace047dd3606c95b5e
Alexander Sosedkin <monk@unboiled.info>
2021-06-05 03:18:19 +0200

    WIP 8: fetch from gh

--- overlays/lib/nixpkgs.nix
+++ overlays/lib/nixpkgs.nix
@@ -4,10 +4,13 @@
 
 let
   # head of nixos-21.05 as of 2021-06-03
-  pinnedPkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/eaba7870ffc3400eca4407baa24184b7fe337ec1.tar.gz";
+  pinnedPkgsSrc = super.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "eaba7870ffc3400eca4407baa24184b7fe337ec1";
     sha256 = "115disiz4b08iw46cidc7lm0advrxn5g2ldmlrxd53zf03skyb2w";
   };
+
 in
 
 import pinnedPkgsSrc {
